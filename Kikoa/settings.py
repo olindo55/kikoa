d1ae5c52c4830bdff631f9f16261fda1bbc853e9
olindo55 <30605083+olindo55@users.noreply.github.com>
2026-05-10 17:17:16 +0100

    add style.css

--- Kikoa/settings.py
+++ Kikoa/settings.py
@@ -126,5 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 LOGIN_REDIRECT_URL = "event_list"
 # LOGOUT_REDIRECT_URL = "login"
